c7a013ad161a9de9bda70bfdd60d227285d901ae
juan ignacio <nho13@hotmail.com>
2024-10-28 10:57:09 +0100

    feat: variables

--- src/main/java/org/example/PrimitiveTypes.java
+++ src/main/java/org/example/PrimitiveTypes.java
@@ -4,5 +4,20 @@ public class PrimitiveTypes {
     public static void main(String[] args) {
         boolean isActive = true;
         System.out.println(isActive);
+        byte primer = 0;
+        System.out.println(primer);
+        short corto = 66;
+        System.out.println(corto);
+        int variable = 455;
+        System.out.println(variable);
+        long largo = 3243324;
+        System.out.println(largo);
+        double doble = 34.56;
+        System.out.println(doble);
+        float flotante = 42.34f;
+        System.out.println(flotante);
+        char letras = 'S';
+        System.out.println(letras);
+
     }
 }
