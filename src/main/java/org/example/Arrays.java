245704ae9fd80befdc20433d4406675610e9d96a
juan ignacio <nho13@hotmail.com>
2024-10-28 14:14:15 +0100

    arrays

---
+++ src/main/java/org/example/Arrays.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Arrays {
+    public static void main(String[] args) {
+        //definir un array de 5 numeros y un array de 5 ciudades
+        int[] numeros = {15, 20, 35, 40, 50};
+        String[] ciudades = {"malaga", "madrid", "barcelona", "granada", "alicante"};
+
+        for (int numero : numeros) {
+            System.out.println("numeros :" + numero);
+
+        }
+        for (String ciudad : ciudades) {
+            System.out.println("ciudad : " + ciudad);
+        }
+    }
+}
