ddd6de87ffc7a5ec91b6b659c38f0b0d9bb4253c
juan ignacio <nho13@hotmail.com>
2024-10-28 12:53:20 +0100

    bucles

---
+++ src/main/java/org/example/FoorLoop.java
@@ -0,0 +1,36 @@
+package org.example;
+
+public class FoorLoop {
+    public static void main(String[] args) {
+
+        //imprimir por consola numeros del 1 al 10 usando un for
+
+        int numero = 10;
+
+        for (int i = 0; i < numero; i++) {
+            System.out.println(numero + "x" + i + "=" + (numero - i));
+        }
+        //imprimir solo numeros pares del 5 a 10
+
+        for (int i = 5; i < 11; i++) {
+            System.out.println("number : " + i);
+        }
+        for (int i = 5; i < 11; i++) {
+            if (i % 2 == 0) {
+                System.out.println("es un numero par : " + i);
+            }
+
+        }
+        //imprimir los numeros impares del 5 al 10 usando while
+
+        int i = 5;
+        while (i <= 10) {
+            if (i % 2 != 0) {
+            }
+            System.out.println("es un numero impar " + i);
+
+
+        } i++;
+    }
+
+}
