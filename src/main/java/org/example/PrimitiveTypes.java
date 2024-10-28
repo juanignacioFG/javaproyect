05c6b8e1c0a76c6d679a30596c15456617c0ae0f
juan ignacio <nho13@hotmail.com>
2024-10-28 10:30:40 +0100

    feat : boolean

---
+++ src/main/java/org/example/PrimitiveTypes.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class PrimitiveTypes {
+    public static void main(String[] args) {
+        boolean isActive = true;
+        System.out.println(isActive);
+    }
+}
